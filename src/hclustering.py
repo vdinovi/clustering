56989ef15a5d4ad03998cdb7ff978b6a92c578c1
vdinovi <vito.dinovi@gmail.com>
2018-05-10 13:15:12 -0700

    added implementation of centroid/wards for updated dist_mat method

--- src/hclustering.py
+++ src/hclustering.py
@@ -3,6 +3,7 @@ from argparse import ArgumentParser
 from os import path
 from datetime import datetime
 import numpy as np
+import math
 from pprint import pprint
 import sys
 import json
@@ -27,35 +28,53 @@ class Node:
             node["nodes"] = [child.to_dict() for child in self.data]
         return node
 
-def single_link(c1, c2, dist_mat):
+def single_link(c1, c2, dist_mat, clusters, data):
+    # clusters, data unused -- passed for consistency with other methods
     for row in range(0, len(dist_mat)):
         dist_mat[row, c1] = min(dist_mat[row, c1], dist_mat[row, c2])
     dist_mat = np.delete(dist_mat, c2, axis=0)
     dist_mat = np.delete(dist_mat, c2, axis=1)
     return dist_mat
 
-def complete_link(c1, c2, dist_mat):
+def complete_link(c1, c2, dist_mat, clusters, data):
+    # clusters, data unused -- passed for consistency with other methods
     for row in range(0, len(dist_mat)):
         dist_mat[row, c1] = max(dist_mat[row, c1], dist_mat[row, c2])
     dist_mat = np.delete(dist_mat, c2, axis=0)
     dist_mat = np.delete(dist_mat, c2, axis=1)
     return dist_mat
 
-#def average_link(c1, c2, dist_mat):
-#    total = 0.0
-#    count = 0
-#    for a in c1:
-#        total += sum([dist_mat[b] for b in c2])
-#    return total / count
-#def centroid(c1, c2):
-#    centroid1 = sum(c1) / float(len(c1))
-#    centroid2 = sum(c2) / float(len(c2))
-#    return sum(abs(centroid2 - centroid1))
-#def wards(c1, c2):
-#    centroid1 = sum(c1) / float(len(c1))
-#    centroid2 = sum(c2) / float(len(c2))
-#    coef = ((len(c1) * len(c2)) / float(len(c1) + len(c2)))
-#    return coef * sum((centroid2 - centroid1)) ** 2
+
+def average_link(c1, c2, dist_mat, clusters, data):
+    # clusters, data unused -- passed for consistency with other methods
+    for row in range(0, len(dist_mat)):
+        dist_mat[row, c1] = (dist_mat[row, c1] + dist_mat[row, c2]) / 2
+    dist_mat = np.delete(dist_mat, c2, axis=0)
+    dist_mat = np.delete(dist_mat, c2, axis=1)
+    return dist_mat
+
+def centroid(c1, c2, dist_mat, clusters, data):
+    # This methods require reading the data to compute centroids
+    cluster = clusters[c1] + clusters[c2]
+    centroid = sum([data.values[p] for p in cluster]) / len(cluster)
+    for row in range(0, len(dist_mat)):
+        orig = sum([data.values[p] for p in clusters[row]]) / len(clusters[row])
+        dist_mat[row, c1] = sum(abs(orig - centroid))
+    dist_mat = np.delete(dist_mat, c2, axis=0)
+    dist_mat = np.delete(dist_mat, c2, axis=1)
+    return dist_mat
+
+def wards(c1, c2, dist_mat, clusters, data):
+    # This methods require reading the data to compute centroids
+    cluster = clusters[c1] + clusters[c2]
+    centroid = sum([data.values[p] for p in cluster]) / len(cluster)
+    for row in range(0, len(dist_mat)):
+        orig = sum([data.values[p] for p in clusters[row]]) / len(clusters[row])
+        coef = len(clusters[row]) * len(cluster) / float(len(clusters[row]) + len(cluster))
+        dist_mat[row, c1] = coef * math.sqrt(sum(orig - centroid) ** 2)
+    dist_mat = np.delete(dist_mat, c2, axis=0)
+    dist_mat = np.delete(dist_mat, c2, axis=1)
+    return dist_mat
 
 def distance_matrix(data):
     clusters = [(di,) for di in range(0, len(data))]
@@ -79,11 +98,11 @@ def generate(data, dist_func):
     tree = [Node("LEAF", 0, clusters[ci][0]) for ci in range(0, len(clusters))]
     while len(clusters) > 1:
         c1, c2, dist = find_closest(dist_mat)
+        dist_mat = dist_func(c1, c2, dist_mat, clusters, data)
         tree[c1] = Node("NODE", dist, (tree[c1], tree[c2]))
         tree.remove(tree[c2])
         clusters[c1] += clusters[c2]
         clusters.pop(c2)
-        dist_mat = dist_func(c1, c2, dist_mat)
     tree[0].node_type = "ROOT"
     return tree[0]
 
@@ -111,7 +130,7 @@ if __name__ == "__main__":
     parser = ArgumentParser(description="A heirarchical clustering program using the agglomeration method. Uses regular manhatten distance for point-distance calucations.")
     parser.add_argument("filename", help="Name of input file in csv form. Note that the first line of this file must be a restrictions vector.")
     parser.add_argument("--threshold", help="Specify a threshold value for which to stop agglomeration. By default, will produce the full cluster heirachy")
-    parser.add_argument("--link-method", default="SINGLE", help="Specify link-method for agglomeration. Allowed values: SINGLE | COMPLETE | AVERAGE | CENTROID | WARD]. By default SINGLE.")
+    parser.add_argument("--link-method", default="SINGLE", help="Specify link-method for agglomeration. Allowed values: SINGLE | COMPLETE | AVERAGE | CENTROID | WARDS]. By default SINGLE.")
     parser.add_argument("--headers", help="Name of input file containing header names on a single line in CSV format. Ommitting this will produce plots with unnamed axis")
     args = parser.parse_args()
     headers = None
@@ -121,10 +140,10 @@ if __name__ == "__main__":
 
     link_methods = {
         "SINGLE": single_link,
-        "COMPLETE": complete_link
-        #"AVERAGE": average_link
-        #"CENTROID": centroid,
-        #"WARD": wards
+        "COMPLETE": complete_link,
+        "AVERAGE": average_link,
+        "CENTROID": centroid,
+        "WARDS": wards
     }
 
     # Generate heirarchy tree
